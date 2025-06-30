512cfb3c2bb0cc87ff6f0eddd38b62b489df5012
maryamhelal <maryam.helaal22@gmail.com>
2025-06-30 08:58:33 +0300

    APIs and tests done

---
+++ main.py
@@ -0,0 +1,173 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+
+from models import *
+from database import engine, create_db, sqlite_file_name
+from database_seeder import create_tasks
+from sqlmodel import Session, select
+from datetime import datetime
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import ValidationError
+import uvicorn
+
+# API Endpoints
+app = FastAPI()
+
+# Create database and different tasks
+@app.post("/seed")
+async def seeder():
+    main()
+    return JSONResponse(status_code=201, content="Database seeded")
+
+# Get all endpoints
+@app.get("/")
+def root():
+    return{"endpoints": ["/health", "/tasks", "/seed"]}
+
+# Get health status
+@app.get("/health")
+def health():
+    return{"status": "OK"}
+
+# Create a task
+@app.post("/tasks")
+async def create_task(task: TaskCreate):
+    try:
+        task_data = Task(**task.model_dump())
+        with Session(engine) as session:
+            session.add(task_data)
+            session.commit()
+            session.refresh(task_data)
+            response = TaskResponse.model_validate(task_data, from_attributes=True).model_dump(mode="json")
+            return JSONResponse(status_code=201, content=response)
+    except ValidationError as e:
+        raise HTTPException(status_code = 422, detail = str(e))
+    except Exception as e:
+        raise HTTPException(status_code = 400, detail = "An error occured, try again")
+
+# Get all tasks
+@app.get("/tasks")
+def get_tasks():
+    try:
+        with Session(engine) as session:
+            statement = select(Task).offset(0).limit(5) # Shows only 5 records, with an offset of 0 starting from the first record
+            tasks = session.exec(statement).all()
+            response = [TaskResponse.model_validate(task, from_attributes=True).model_dump(mode="json") for task in tasks]
+            return JSONResponse(status_code=200, content=response)
+    except Exception as e:
+        raise HTTPException(status_code = 400, detail = "An error occured, try again")
+
+# Get a specific task with id
+@app.get("/tasks/{task_id}")
+def get_task_with_id(task_id: int):
+    try:
+        with Session(engine) as session:
+            task = session.get(Task, task_id)
+            if not task:
+                raise HTTPException(status_code = 404, detail = "Task not found")
+            response = TaskResponse.model_validate(task, from_attributes=True).model_dump(mode="json")
+            return JSONResponse(status_code=200, content=response)
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code = 400, detail = "An error occured, try again")
+
+# Update a specific task with id
+@app.put("/tasks/{task_id}")
+async def update_task_with_id(task_id: int, tasknew: TaskUpdate):
+    try:
+        with Session(engine) as session:
+            task = session.get(Task, task_id)
+            if not task:
+                raise HTTPException(status_code = 404, detail = "Task not found")
+            if(tasknew.title):
+                task.title = tasknew.title
+            if(tasknew.description):
+                task.description = tasknew.description
+            if(tasknew.status):
+                task.status = tasknew.status
+            if(tasknew.priority):
+                task.priority = tasknew.priority
+            if(tasknew.due_date):
+                task.due_date = tasknew.due_date
+            if(tasknew.assigned_to):
+                task.assigned_to = tasknew.assigned_to
+
+            task.updated_at = datetime.now()
+            session.add(task)
+            session.commit()
+            session.refresh(task)
+            response = TaskResponse.model_validate(task, from_attributes=True).model_dump(mode="json")
+            return JSONResponse(status_code=200, content=response)
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code = 400, detail = "An error occured, try again")
+    
+# Delete a specific task with id
+@app.delete("/tasks/{task_id}")
+def delete_task_with_id(task_id: int):
+    with Session(engine) as session:
+        task = session.get(Task, task_id)
+        if not task:
+            raise HTTPException(status_code = 404, detail = "Task not found")
+        session.delete(task)
+        session.commit()
+
+        task = session.get(Task, task_id)
+        if(not task):
+            return JSONResponse(status_code=200, content="File deleted successfully")
+        else:
+            raise HTTPException(status_code = 400, detail = "Could not delete task")
+
+# Filter tasks based on status
+@app.get("/tasks/status/{status}")
+def get_tasks_with_status(status: TaskStatus):
+    try:
+        with Session(engine) as session:
+            statement = select(Task).where(Task.status == status)
+            tasks = session.exec(statement).all()
+            response = [TaskResponse.model_validate(task, from_attributes=True).model_dump(mode="json") for task in tasks]
+            return JSONResponse(status_code=200, content=response)
+    except Exception as e:
+        raise HTTPException(status_code = 400, detail = "An error occured, try again")    
+
+# Filter tasks based on priority
+@app.get("/tasks/priority/{priority}")
+def get_tasks_with_priority(priority: TaskPriority):
+    try:
+        with Session(engine) as session:
+            statement = select(Task).where(Task.priority == priority)
+            tasks = session.exec(statement).all()
+            response = [TaskResponse.model_validate(task, from_attributes=True).model_dump(mode="json") for task in tasks]
+            return JSONResponse(status_code=200, content=response)
+    except Exception as e:
+        raise HTTPException(status_code = 400, detail = "An error occured, try again")
+
+# Filter tasks based on both status and priority
+@app.get("/tasks/status/{status}/priority/{priority}")
+def get_tasks_with_priority(status: TaskStatus, priority: TaskPriority):
+    try:
+        with Session(engine) as session:
+            statement = select(Task).where(Task.status == status, Task.priority == priority).limit(10) # Shows only 10 records
+            tasks = session.exec(statement).all()
+            response = [TaskResponse.model_validate(task, from_attributes=True).model_dump(mode="json") for task in tasks]
+            return JSONResponse(status_code=200, content=response)
+    except Exception as e:
+        raise HTTPException(status_code = 400, detail = "An error occured, try again")  
+
+# Delete old database if exists, and create database and sample data
+def main(): 
+    engine.dispose()
+    if os.path.exists(sqlite_file_name):
+        os.remove(sqlite_file_name)
+    create_db()
+    create_tasks()
+
+# For running the application, call main method and run fastapi
+if __name__=="__main__":
+    main()
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
---
+++ test_apis.py
@@ -0,0 +1,159 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+
+from fastapi.testclient import TestClient
+import pytest
+from main import app
+from database import sqlite_file_name, engine
+
+client = TestClient(app)
+
+# 1. Test seeding
+def test_seeding_returns_success():
+    response = client.post("/seed")
+    assert response.status_code == 201
+
+# 2. Test get endpoints
+def test_get_endpoints_returns_success():
+    response = client.get("/")
+    assert response.status_code == 200
+
+# 3. Test get health status
+def test_get_health_returns_success():
+    response = client.get("/health")
+    assert response.status_code == 200
+
+# 4. Test create task with correct input
+def test_post_tasks_returns_success():
+    response = client.post(
+        "/tasks",
+        json={
+            "title": "string", 
+            "description": "string",
+            "due_date": "2025-08-30T05:02:21.437Z",
+            "assigned_to": "string"})
+    assert response.status_code == 201
+
+# 5. Test create task with incorrect input (empty title)
+def test_post_tasks_empty_title_returns_validation_error():
+    response = client.post(
+        "/tasks",
+        json={"title": ""})
+    assert response.status_code == 422
+
+# 6. Test create task with incorrect input (only whitespaced title)
+def test_post_tasks_whitespaced_title_returns_validation_error():
+    response = client.post(
+        "/tasks",
+        json={"title": " "})
+    assert response.status_code == 422
+
+# 7. Test create task with incorrect input (due date with a past date)
+def test_post_tasks_past_due_date_returns_validation_error():
+    response = client.post(
+        "/tasks",
+        json={"title": "Test Task", "due_date": "2024-02-05T02:04:56.916Z"})
+    assert response.status_code == 422
+
+# 8. Test get tasks
+def test_get_tasks_returns_success():
+    response = client.get("/tasks")
+    assert response.status_code == 200
+
+# 9. Test get task with correct id
+def test_get_task_with_id_returns_success():
+    response = client.get("/tasks/1")
+    assert response.status_code == 200
+
+# 10. Test get task with no task corresponding to id
+def test_get_task_with_id_returns_not_found_error():
+    response = client.get("/tasks/400")
+    assert response.status_code == 404
+
+# 11. Test get task with incorrect format of id
+def test_get_task_with_id_returns_validation_error():
+    response = client.get("/tasks/string")
+    assert response.status_code == 422
+
+# 12. Test update task with correct id
+def test_put_task_with_id_returns_success():
+    response = client.put("/tasks/1", json={"assigned_to": "John"})
+    assert response.status_code == 200
+
+# 13. Test update task with incorrect title
+def test_put_task_with_id_returns_validation_error():
+    response = client.put("/tasks/1", json={"title": ""})
+    assert response.status_code == 422
+
+# 14. Test update task with incorrect id
+def test_put_task_with_id_returns_not_found_error():
+    response = client.put("/tasks/720", json={"title": "New"})
+    assert response.status_code == 404
+
+# 15. Test delete task with correct id
+def test_delete_task_with_id_returns_success():
+    response = client.delete("/tasks/2")
+    assert response.status_code == 200
+
+# 16. Test delete task with incorrect id
+def test_delete_task_with_id_returns_not_found_error():
+    response = client.delete("/tasks/200")
+    assert response.status_code == 404
+
+# 17. Test filter tasks using status with correct input
+def test_get_tasks_with_status_returns_success():
+    response = client.get("/tasks/status/pending")
+    assert response.status_code == 200
+
+# 18. Test filter tasks using status with incorrect input
+def test_get_tasks_with_status_returns_validation_error():
+    response = client.get("/tasks/status/pen")
+    assert response.status_code == 422
+
+# 19. Test filter tasks using status to return empty array
+def test_get_tasks_with_status_returns_empty_array():
+    response = client.get("/tasks/status/in_progress")
+    assert response.status_code == 200
+    assert response.json() == []
+
+# 20. Test filter tasks using priority with correct input
+def test_get_tasks_with_priority_returns_success():
+    response = client.get("/tasks/priority/medium")
+    assert response.status_code == 200
+
+# 21. Test filter tasks using priority with incorrect input
+def test_get_tasks_with_priority_returns_validation_error():
+    response = client.get("/tasks/priority/hi")
+    assert response.status_code == 422
+
+# 22. Test filter tasks using priority returns empty array
+def test_get_tasks_with_priority_returns_empty_array():
+    response = client.get("/tasks/priority/high")
+    assert response.status_code == 200
+    assert response.json() == []
+
+# 23. Test filter tasks using status and priority with correct input
+def test_get_tasks_with_status_and_priority_returns_success():
+    response = client.get("/tasks/status/pending/priority/medium")
+    assert response.status_code == 200
+
+# 24. Test filter tasks using status and priority with incorrect input
+def test_get_tasks_with_status_and_priority_returns_validation_error():
+    response = client.get("/tasks/status/prog/priority/med")
+    assert response.status_code == 422
+
+# 25. Test filter tasks using status and priority returns empty array
+def test_get_tasks_with_status_and_priority_returns_empty_array():
+    response = client.get("/tasks/status/completed/priority/low")
+    assert response.status_code == 200
+    assert response.json() == []
+
+# Delete database after tests are done
+@pytest.fixture(scope="session", autouse=True)
+def cleanup_database_after_tests():
+    yield
+    engine.dispose()
+    if os.path.exists(sqlite_file_name):
+        os.remove(sqlite_file_name)
